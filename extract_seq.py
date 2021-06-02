2ff84df902c8428c3238dfa15776d1afe6cd53aa
Sen Cai <cs1159821786@gmail.com>
2021-06-02 20:00:03 +0800

    extract the sequences according to list

---
+++ extract_seq.py
@@ -0,0 +1,21 @@
+with open ('subject.fasta', 'r') as subject_file:
+    subject = {}
+    for line in subject_file:
+        if '>' in line:
+            seq_name = line.strip().strip('>')
+            subject[seq_name] = []
+        else:
+            subject[seq_name].append(line.strip())
+    for key, value in subject.items():
+        subject[key] = ''.join(value)
+
+with open ('query.list', 'r') as query_file:
+    out = {}
+    for line in query_file:
+        if line.strip() in subject:
+            out[line.strip()] = subject[line.strip()]
+
+with open ('query.fasta', 'w') as out_file:
+    for key, value in out.items():
+        out_file.write('>' + key + '\n')
+        out_file.write(value + '\n')
