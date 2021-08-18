b86c532905a49b1ca3029a656670e4e30b3d6337
Sen Cai <cs1159821786@gmail.com>
2021-08-18 20:51:21 +0800

    add get_SNP_depth.py

---
+++ get_SNP_depth.py
@@ -0,0 +1,16 @@
+import gzip
+with gzip.open('ht.vcf.gz', 'rt') as input_file:
+    format = []
+    depth = {}
+    for line in input_file:
+        if line[0] != '#':
+            format = format + (line.split()[9:])
+            for sample in format:
+                dp = sample.split(':')[2]
+                if dp in depth.keys():
+                    depth[dp] = depth[dp] + 1
+                else:
+                    depth[dp] = 0
+
+for key, value in depth.items():
+    print(key, value)
